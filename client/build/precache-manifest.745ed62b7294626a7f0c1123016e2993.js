6bcc0a58fce4a1f4d8cee81f899b4b15dd8ac76a
Daniel guadalupe <danieltguadalupe@gmail.com>
2021-02-24 23:07:23 -0500

    ran npm run build

---
+++ client/build/precache-manifest.745ed62b7294626a7f0c1123016e2993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6971307206d775b09043fad67f89d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "656913ccb0f2b907b391",
+    "url": "/static/css/main.174b53f3.chunk.css"
+  },
+  {
+    "revision": "604abe6dba0aa7559cd6",
+    "url": "/static/js/2.d641fcfe.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.d641fcfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656913ccb0f2b907b391",
+    "url": "/static/js/main.0af2aa72.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
